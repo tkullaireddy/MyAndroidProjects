3d7bae424457760ceb7cce2c74972221d751b228
tkullaireddy <tkullaireddy@gmail.com>
2024-02-17 16:15:16 +0530

    update

--- Multipleactivities/app/src/main/java/com/tadiaptri/multipleactivities/Information.kt
+++ Multipleactivities/app/src/main/java/com/tadiaptri/multipleactivities/Information.kt
@@ -17,15 +17,14 @@ class Information : AppCompatActivity() {
         findViewById<TextView>(R.id.textView).text=GetInformation(name,age)
 }
 
-    fun GetInformation(aName:String, aAge:Int ):String{
+    private fun GetInformation(aName:String, aAge:Int ):String{
         val calendar = Calendar.getInstance()
         val currentYear = calendar.get(Calendar.YEAR)
         val yearBirth = currentYear - aAge
-        var typeofAge = "Young Aged"
-        if (aAge>45) typeofAge = "You are born in $yearBirth, you've accumulated a wealth of experience and wisdom, making you a respected senior figure.\n Your maturity and dedication have undoubtedly led you to acquire a vast amount of knowledge, which is truly impressive."
-        else if (aAge>33) typeofAge = "You are Born in $yearBirth, you've entered the vibrant stage of middle age, bringing with it both experience and maturity.\n Your talents are undeniable, adding another layer of dimension to your life."
-        else if (aAge>24) typeofAge = "You are born in $yearBirth, you embody a youthful spirit while demonstrating continuous growth in various aspects.\n This evolution makes you a truly wonderful companion."
-        else  typeofAge = "You are born in $yearBirth, you've exciting opportunities to explore and grow in many directions. With an open mind and a willingness to learn, you can discover a wealth of knowledge and experiences.\n Enjoy the journey!"
+        val typeofAge = if (aAge>45) "You are born in $yearBirth, you've accumulated a wealth of experience and wisdom, making you a respected senior figure.\n Your maturity and dedication have undoubtedly led you to acquire a vast amount of knowledge, which is truly impressive."
+        else if (aAge>33) "You are Born in $yearBirth, you've entered the vibrant stage of middle age, bringing with it both experience and maturity.\n Your talents are undeniable, adding another layer of dimension to your life."
+        else if (aAge>24) "You are born in $yearBirth, you embody a youthful spirit while demonstrating continuous growth in various aspects.\n This evolution makes you a truly wonderful companion."
+        else "You are born in $yearBirth, you've exciting opportunities to explore and grow in many directions. With an open mind and a willingness to learn, you can discover a wealth of knowledge and experiences.\n Enjoy the journey!"
 
         return  " Hi $aName,\n\n $typeofAge"
     }
