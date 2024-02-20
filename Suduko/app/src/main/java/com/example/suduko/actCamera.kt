cb188b1ad6d679d515edea378ab204d92dc4d840
tkullaireddy <tkullaireddy@gmail.com>
2024-02-20 18:09:33 +0530

    update

---
+++ Suduko/app/src/main/java/com/example/suduko/actCamera.kt
@@ -0,0 +1,11 @@
+package com.example.suduko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class actCamera : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_act_camera)
+    }
+}
