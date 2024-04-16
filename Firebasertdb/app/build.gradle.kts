d2ab3f974df16d4184b094a378ee875a13b6605e
Kullai Reddy <tkullaireddy@gmail.com>
2024-04-16 07:50:26 +0530

    firebasedb updated

--- Firebasertdb/app/build.gradle.kts
+++ Firebasertdb/app/build.gradle.kts
@@ -51,7 +51,6 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.support.annotations)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
